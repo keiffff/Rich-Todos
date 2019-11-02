a9a38976b25e5612f0aeb774d54a3662840b5b0e
kei <tenguuenglish@gmail.com>
2019-11-03 07:04:48 +0900

    :sparkles: Introduce material-ui

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,3 +1,17 @@
 import * as React from 'react';
+import { Card, CardActions, CardContent, CardHeader, Fab } from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 
-export const Hello = () => <h1>hello, world!</h1>;
+export const Hello = () => {
+  return (
+    <Card>
+      <CardHeader>Material-UI</CardHeader>
+      <CardContent>Task</CardContent>
+      <CardActions>
+        <Fab color="primary">
+          <EditIcon />
+        </Fab>
+      </CardActions>
+    </Card>
+  );
+};
