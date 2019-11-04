01a00e9c650ac71f0d055fb3e10b22a20faa9544
kei <tenguuenglish@gmail.com>
2019-11-04 19:23:44 +0900

    :lipstick: Fix styles

--- src/components/TaskLane.tsx
+++ src/components/TaskLane.tsx
@@ -7,26 +7,66 @@ type Props = {
   tasks: Task[];
 };
 
+const baseStyle = css({
+  display: 'flex',
+  flex: 'auto',
+});
+
 const taskListStyle = css({
   width: 328,
+  margin: 0,
+  padding: 0,
   display: 'flex',
   flexDirection: 'column',
-  '> div': {
+  listStyle: 'none',
+  '> li': {
     marginTop: 8,
   },
 });
 
 export const TaskLane = ({ tasks }: Props) => {
+  const todoTasks = tasks.filter(task => task.status === 'TODO');
+  const inProgressTasks = tasks.filter(task => task.status === 'IN_PROGRESS');
+  const doneTasks = tasks.filter(task => task.status === 'DONE');
+
   return (
-    <>
-      <header>status</header>
-      <div className={taskListStyle}>
-        {tasks.map(task => (
-          <div draggable>
-            <TaskCard task={task} />
-          </div>
-        ))}
+    <section className={baseStyle}>
+      <div>
+        <header>
+          <span>Todo</span>
+        </header>
+        <ul className={taskListStyle}>
+          {todoTasks.map(task => (
+            <li draggable>
+              <TaskCard task={task} />
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+      <div>
+        <header>
+          <span>In progress</span>
+        </header>
+        <ul className={taskListStyle}>
+          {inProgressTasks.map(task => (
+            <li draggable>
+              <TaskCard task={task} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <header>
+          <span>Done</span>
+        </header>
+        <ul className={taskListStyle}>
+          {doneTasks.map(task => (
+            <li draggable>
+              <TaskCard task={task} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
