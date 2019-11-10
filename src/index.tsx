44442702e1435416edc8c8b9ad03ea6681b76cf8
kei <tenguuenglish@gmail.com>
2019-11-11 04:04:47 +0900

    :wrench: Initial Firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { firebaseConfig } from './FirebaseConfig';
 import { TaskIndexContainer } from './containers/TaskIndex';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<TaskIndexContainer />, document.getElementById('app'));
