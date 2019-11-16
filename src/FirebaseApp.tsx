f11336a7caa729ce4409925de022032c8695a74e
kei <tenguuenglish@gmail.com>
2019-11-16 11:16:41 +0900

    :recycle: Fix dir structure

--- src/FirebaseApp.tsx
+++ src/FirebaseApp.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import { FirebaseContext } from './contexts';
+import { FirebaseContext } from './contexts/firebase';
 
 type Props = {
   children: React.ReactNode;
